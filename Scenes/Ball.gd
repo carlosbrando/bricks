18c3f39b85671fa5d94cc3795258db26a4782a68
Carlos Brando <brando@enjoei.com.br>
2020-02-05 14:44:50 -0300

    Add ball+paddle physics.

--- Scenes/Ball.gd
+++ Scenes/Ball.gd
@@ -1,8 +1,20 @@
 extends RigidBody2D
 
-func _physics_process(delta):
+export var speedup = 400
+export var maxspeed = 30000
+
+func _physics_process(delta):	
 	var bodies = get_colliding_bodies()
 	
 	for body in bodies:
 		if body.is_in_group("Bricks"):
 			body.queue_free()
+
+		elif body.name == "Paddle":
+			var speed = linear_velocity.length()
+			var direction = position - body.get_node("Anchor").global_position
+			var velocity = direction.normalized() * min(speed + speedup * delta, maxspeed * delta)
+			linear_velocity = velocity
+
+	if position.y > get_viewport_rect().end.y:
+		queue_free()
--- Scenes/Paddle.gd
+++ Scenes/Paddle.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var y = position.y
 	var x = get_viewport().get_mouse_position().x
 	
